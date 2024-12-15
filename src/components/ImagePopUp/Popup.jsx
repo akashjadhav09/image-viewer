fef473ef34546df91e712a559587044f48c1aaa3
akashjadhav09 <152849719+akashjadhav09@users.noreply.github.com>
2024-12-15 15:10:13 +0530

    Disable background click when pop up is opened.

--- src/components/ImagePopUp/Popup.jsx
+++ src/components/ImagePopUp/Popup.jsx
@@ -2,13 +2,12 @@ import { MdClose } from "react-icons/md";
 
 import './popup.css'
 
-function ImagePopUpModal({isOpen, onClose, title, imagePath, imageSize, imageExtension}) {
-
+function ImagePopUpModal({isOpen, onClose, title, imagePath, imageSize, imageExtension, isDark}) {
     if (!isOpen) return null;
 
     return (
-        <div className="popup-wrapper-outer">            
-            <div className='popup-wrapper-inner' onClick={(e) => e.stopPropagation()}>
+        <div className="popup-wrapper-outer">          
+            <div className="popup-wrapper-inner" onClick={(e) => e.stopPropagation()} style={isDark ? { backgroundColor: '#121212', color: '#E3E3E3' } : { backgroundColor: 'white', color: 'black' }}>
                 <div className="close-button-wrapper-outer">
                     <div className="image-preview">Image Preview</div>
                     <div className="close-button-wrapper-inner">
@@ -16,7 +15,7 @@ function ImagePopUpModal({isOpen, onClose, title, imagePath, imageSize, imageExt
                     </div>
                 </div>
                 <div className='image-name-container'>
-                    <input type="text" className="image-alt-text" value={title || "Default Image"} readOnly/>
+                    <input type="text" className="image-alt-text" value={title || "Default Image"} style={isDark ? {backgroundColor : '#202020', color: "white"} : {backgroundColor: 'white', color: 'black'}} readOnly/>
                 </div>
                 <div className='image-container'>
                     <img src={imagePath}/>
